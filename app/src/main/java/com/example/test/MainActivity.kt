f2c33c467f666b5479d80e2f1014cfd2a0d79c10
Влад Кантамиров <kantamirov2016@gmail.com>
2024-03-04 22:08:45 +0300

    add function for authorization and registration

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -7,10 +7,12 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var name: EditText
+    lateinit var password: EditText
     lateinit var signNow: TextView
     lateinit var signIn: Button
 
@@ -19,14 +21,28 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         signIn = findViewById(R.id.buttonSignIn)
-        name = findViewById(R.id.editTextAccount)
+        name = findViewById(R.id.editTextAccountAuthorization)
+        password = findViewById(R.id.editTextPassword)
         signNow = findViewById(R.id.textView3)
 
-
         signIn.setOnClickListener {
-            val intent: Intent = Intent(this@MainActivity, hello::class.java)
-            intent.putExtra("userName", name.text.toString())
-            startActivity(intent)
+
+            val userNameValid: String = name.text.toString().trim()
+            val passwordValid: String = password.text.toString().trim()
+
+            if (userNameValid.isEmpty())
+                Toast.makeText(applicationContext, "Поле логина не введено", Toast.LENGTH_SHORT).show()
+            else if (userNameValid.length < 5)
+                Toast.makeText(applicationContext, "Поле логина аккаунта не может быть меньше 5 символов", Toast.LENGTH_SHORT).show()
+            else if (passwordValid.isEmpty())
+                Toast.makeText(applicationContext, "Поле пароля не введено", Toast.LENGTH_SHORT).show()
+            else if (passwordValid.length < 8)
+                Toast.makeText(applicationContext,"Пароль меньше 6 символов", Toast.LENGTH_SHORT).show()
+            else{
+                val intent: Intent = Intent(this@MainActivity, hello::class.java)
+                intent.putExtra("userName", name.text.toString())
+                startActivity(intent)
+            }
         }
         signNow.setOnClickListener {
             val intent: Intent = Intent(this@MainActivity, Registration::class.java)
--- app/src/main/java/com/example/test/Registration.kt
+++ app/src/main/java/com/example/test/Registration.kt
@@ -2,11 +2,12 @@ package com.example.test
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import java.util.regex.Pattern
 
 class Registration : AppCompatActivity() {
 
@@ -15,29 +16,54 @@ class Registration : AppCompatActivity() {
     private lateinit var password: EditText
     private lateinit var repeatPassword: EditText
     private lateinit var regBtn: Button
+    private lateinit var backTextView: TextView
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
 
-        userName = findViewById(R.id.editTextAccount)
+        userName = findViewById(R.id.editTextAccountRegistration)
         email = findViewById(R.id.editTextEmail)
         password = findViewById(R.id.editTextPassword)
         repeatPassword = findViewById(R.id.editTextRepeatPassword)
         regBtn = findViewById(R.id.buttonReg)
+        backTextView = findViewById(R.id.textViewBackRegistration)
+
 
         regBtn.setOnClickListener{
-            if (userName.text.toString().trim().isEmpty()) Toast.makeText(applicationContext, "Поле логина не заполнено" , Toast.LENGTH_SHORT).show()
-            else if (email.text.toString().trim().isEmpty()) Toast.makeText(applicationContext, "Поле почтового адреса не заполнено", Toast.LENGTH_SHORT).show()
-            else if (password.text.toString().trim().isEmpty()) Toast.makeText(applicationContext, "Поле пароля не введено", Toast.LENGTH_SHORT).show()
-            else if (password.text.toString().trim().length < 6) Toast.makeText(applicationContext,"Пароль меньше 6 символов", Toast.LENGTH_SHORT).show()
-            else if (repeatPassword.text.toString().trim().isEmpty()) Toast.makeText(applicationContext, "Поле пароля (повторное) не введено", Toast.LENGTH_SHORT).show()
-            else if (password.text.toString().trim() != repeatPassword.text.toString().trim()) Toast.makeText(applicationContext, "Пароли не совпадают", Toast.LENGTH_SHORT).show()
+
+            val userNameValid: String = userName.text.toString().trim()
+            val emailValid: String = email.text.toString().trim()
+            val passwordValid: String = password.text.toString().trim()
+            val repeatPasswordValid: String = repeatPassword.text.toString().trim()
+
+            if (userNameValid.isEmpty())
+                Toast.makeText(applicationContext, "Поле логина не заполнено" , Toast.LENGTH_SHORT).show()
+            else if (userNameValid.length < 5)
+                Toast.makeText(applicationContext, "Поле логина аккаунта не может быть меньше 5 символов", Toast.LENGTH_SHORT).show()
+            else if (emailValid.isEmpty())
+                Toast.makeText(applicationContext, "Поле почтового адреса не заполнено", Toast.LENGTH_SHORT).show()
+            else if (!android.util.Patterns.EMAIL_ADDRESS.matcher(emailValid).matches())
+                Toast.makeText(applicationContext,  "Неверный формат почтового адреса", Toast.LENGTH_SHORT).show()
+            else if (passwordValid.isEmpty())
+                Toast.makeText(applicationContext, "Поле пароля не введено", Toast.LENGTH_SHORT).show()
+            else if (passwordValid.length < 8)
+                Toast.makeText(applicationContext,"Пароль меньше 6 символов", Toast.LENGTH_SHORT).show()
+            else if (repeatPasswordValid.isEmpty())
+                Toast.makeText(applicationContext, "Поле пароля (повторное) не введено", Toast.LENGTH_SHORT).show()
+            else if (passwordValid != repeatPasswordValid)
+                Toast.makeText(applicationContext, "Пароли не совпадают", Toast.LENGTH_SHORT).show()
             else {
                 val intent: Intent = Intent(this, hello::class.java)
                 intent.putExtra("userName", userName.text.toString())
                 startActivity(intent)
             }
         }
+
+        backTextView.setOnClickListener {
+            val intent: Intent = Intent(this@Registration,MainActivity::class.java)
+            startActivity(intent)
+        }
     }
+
 }
